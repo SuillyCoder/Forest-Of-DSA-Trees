2244a817c40a103692166ce427aa0783bdd70405
Enzo Basuil <146545169+SuillyCoder@users.noreply.github.com>
2024-05-05 22:03:52 +0800

    Update deletion.c

--- Graph Tree/deletion.c
+++ Graph Tree/deletion.c
@@ -5,24 +5,25 @@
 void deleteVertex(struct graph *graphTree, int deletor){
     int foundIt = 0;
     unsigned long long v;
-    for (v = -1; v < graphTree->vertexCount; v++){
+    for (v = 0; v < graphTree->vertexCount; v++){
             struct vertex *current = graphTree->adjLists[v];
             struct vertex *previous = NULL;
             if (current != NULL){
             while (current){
-                if (current->value == deletor && previous == NULL){
+                if (v == deletor) {
                     graphTree->adjLists[v] = current->next;
-                    free(current);
-                    foundIt = 1;
+                    current = graphTree->adjLists[v];
                 }
                 else if (current->value == deletor){
                     if (previous == NULL){
-                        graphTree->adjLists[v] = current->next;
+                          graphTree->adjLists[v] = current->next;
                     }
                     else{
-                         previous->next = current->next;
+                        previous->next = current->next;
                     }
-                    free(current);
+                    struct vertex *temp = current;
+                    current = current->next;
+                    free(temp);
                     foundIt = 1;
                 }
                 else{
@@ -33,7 +34,7 @@ void deleteVertex(struct graph *graphTree, int deletor){
         }
     }
 
-    if (!foundIt) {
+    if (foundIt != 1) {
         printf("\n\nThis vertex does not exist within the Graph!\n\n");
     }
     else{
